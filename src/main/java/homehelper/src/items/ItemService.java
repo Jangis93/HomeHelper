3f855c651a6c8149c8f63258ea51edfef72038f1
michaela <michaela.janegfalk@gmail.com>
2023-06-04 15:50:56 +0200

    add methods to fetch shopping items.

--- src/main/java/homehelper/src/items/ItemService.java
+++ src/main/java/homehelper/src/items/ItemService.java
@@ -3,6 +3,7 @@ package homehelper.src.items;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,5 +15,13 @@ public class ItemService {
     public Optional create(ItemRequest request) {
         ShoppingItem newItem = request.map();
         return Optional.of(itemRepository.save(newItem));
+    }
+
+    public Optional<ShoppingItem> getItem(Integer id) {
+        return itemRepository.findById(id);
+    }
+
+    public List<ShoppingItem> getAll() {
+        return (List<ShoppingItem>) itemRepository.findAll();
     }
 }
